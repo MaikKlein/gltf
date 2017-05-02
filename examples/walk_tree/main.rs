3dea3f8c43fdb6a6efbecd90e0e6e65fbeb1866c
alteous <alteous@outlook.com>
2017-05-02 11:24:09 +0100

    Fixed examples

--- examples/walk_tree/main.rs
+++ examples/walk_tree/main.rs
@@ -7,11 +7,14 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate gltf;
+extern crate gltf as gltf_crate;
 
-type Extras = gltf::v2::extras::None;
+use gltf_crate::v2 as gltf;
 
-fn visit_nodes(node: &gltf::v2::tree::scene::Node<Extras>, level: u32) {
+type Extras = gltf::extras::None;
+type Gltf = gltf::Root<Extras>;
+
+fn visit_nodes(node: &gltf::scene::Node<Extras>, level: u32) {
     println!("Node {}", level);
     for child in node.iter_child_nodes() {
         visit_nodes(&child, level + 1);
@@ -20,8 +23,8 @@ fn visit_nodes(node: &gltf::v2::tree::scene::Node<Extras>, level: u32) {
 
 fn main() {
     let path = "glTF-Sample-Models/2.0/Lantern/glTF/Lantern.gltf";
-    let gltf = gltf::v2::import::<_, Extras>(path).unwrap();
-    for scene in gltf.tree().unwrap().iter_scenes() {
+    let gltf: Gltf = gltf::import(path).unwrap();
+    for scene in gltf.iter_scenes() {
         for node in scene.iter_nodes() {
             visit_nodes(&node, 1);
         }
